20a6983cbc81812d5bf340c90ce228381169a179
Xuelu Feng <gcctryg@gmail.com>
2021-06-09 15:20:51 -0700

    added methods and tests

---
+++ test.py
@@ -0,0 +1,21 @@
+import unittest
+import example
+
+
+class TestCase(unittest.TestCase):
+
+    def test_add_1(self):
+        self.assertEqual(example.add(1, 2), 3)
+
+    def test_subtract_1(self):
+        self.assertEqual(example.subtract(1, 1), 0)
+
+    def test_multiply_1(self):
+        self.assertEqual(example.multiply(2, 2), 4)
+
+    def test_division_1(self):
+        self.assertEqual(example.division(6, 2), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
